b41948d659fb4d393b2e7dbc813a5c554b25b54b
Matthew Ogtong <ogtongm@gmail.com>
2026-02-26 13:08:30 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Matthew Ogtong on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
